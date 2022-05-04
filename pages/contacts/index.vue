01e76fac698d67ad0371b257b5a99dfb4f51582f
Stefano Franceschetto <stefano.franceschetto@moba.studio>
2022-05-04 11:39:00 +0200

    all pages up desktop

--- pages/contacts/index.vue
+++ pages/contacts/index.vue
@@ -1,15 +1,31 @@
 <template>
   <div>
-    
+    <div class="px-6 h-[600px]">
+      <div
+        class="h-full rounded-b-xl bg-[url(/images/contacts1.jpg)] bg-center bg-cover bg-no-repeat"
+      />
+    </div>
+
+    <div class="mt-12 container">
+      <h1 class="w-1/2 text-7xl">BB Network, leaders in Rome student housing.</h1>
+
+      <div class="mt-12">
+        <p class="font-bold">BB Network srl</p>
+        <p>Piazzale Gregorio VII 39</p>
+        <p>00164 Roma</p>
+        <p class="font-bold underline">COME RAGGIUNGERCI</p>
+      </div>
+
+      <div class="mt-12">
+        <p>E. <a class="font-bold underline" href="mailto:info@bb.network">INFO@BB.NETWORK</a> </p>
+        <p>T. <a class="font-bold underline" href="tel:+39063334567">+39 063334567</a> </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
